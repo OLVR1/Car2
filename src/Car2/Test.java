efd5ab63bb1cc47058d1504028fb6dc0dd5744b1
oliver <oliverseck@hotmail.com>
2019-10-03 00:05:55 +0200

    Commit2

--- src/Car2/Test.java
+++ src/Car2/Test.java
@@ -7,14 +7,16 @@ public class Test {
         Bil b2 = new Bil("Citroën", "[SV 27699]", "rød");
 
 
-        System.out.println(b1 + "\n" + b1.getRegno() + "\n" + "Næste ledige serienr:" + b1.getNextregno());
-        System.out.println("\n" + b2 + "\n" + b1.getRegno() + "\n" + "Næste ledige serienr:" + b1.getNextregno());
+        System.out.println(b1 + "\n" + "Serienr: " + b1.getRegno() + "\n" + "Næste ledige serienr: " + b1.getNextregno());
+        System.out.println("\n" + b2 + "\n" + "Serienr: " +  b1.getRegno() + "\n" + "Næste ledige serienr: " + b1.getNextregno());
 
         if (b1.alike(b2))
             System.out.println("\n" + "Bilerne er ens");
 
         if (b1.equals(b2))
             System.out.println("\n" + "Nummerpladen er ens");
+
+        System.out.println("\n" + "Næste ledige serienr: " + b1.getNextregno());
     }
 }
 
